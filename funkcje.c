1de538d0815f3d89ce3d71b28215c7ddb61a2c8f
Strzelacz48 <strzelacz48@gmail.com>
2021-01-12 13:15:32 +0100

    initial commit

---
+++ funkcje.c
@@ -0,0 +1,14 @@
+#include"naglowek.h"
+typedef struct elem
+{
+    int val;
+    int x;
+    int y;
+} kafelek;
+bool check(kafelek a,kafelek b)
+{
+if(a.val==b.val)
+    return true;
+else
+    return false;
+}
---
+++ naglowek.h
@@ -0,0 +1,8 @@
+#ifndef NAGLOWEK_H_INCLUDED
+#define NAGLOWEK_H_INCLUDED
+#include<stdio.h>
+#include<stdlib.h>
+#include<gtk/gtk.h>
+#include<stdbool.h>
+
+#endif // NAGLOWEK_H_INCLUDED
