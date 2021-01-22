787d51ba0773896a7c5f4dfe964a7e648b68a834
Strzelacz48 <strzelacz48@gmail.com>
2021-01-22 21:12:18 +0100

    Gra wykrywa koniec rozgrywki i nie pozwala wybierać odkrytych pól

--- main.c
+++ main.c
@@ -3,11 +3,12 @@ int main()
 {
     kafelek plansza[6][6];
     mieszanie(plansza);
-    int tab[18]={0};
-    for(int i=0;i<6;i++)
+    int tab[18]= {0};
+    for(int i=0; i<6; i++)
     {
-        for(int j=0;j<6;j++)
-        {//sprawdzanie czy są pary/*
+        for(int j=0; j<6; j++)
+        {
+            //sprawdzanie czy są pary/*
             if(tab[plansza[i][j].val]==2)
             {
                 printf("error");
@@ -21,12 +22,17 @@ int main()
         }
         printf("\n");
     }//pom ilosć aktualnie wybranych kafelków
+    for(int i=0; i<18; i++) //resetowanie tabeli z ilością par
+    {
+        tab[i]=0;
+    }
     int x,y,x1,y1,pom=0;
     while(true)
     {
-        for(int i=0;i<6;i++)
+        //drukowanie planszy *- zakryte, pole numer - odkryte pole ze znakiem
+        for(int i=0; i<6; i++)
         {
-            for(int j=0;j<6;j++)
+            for(int j=0; j<6; j++)
             {
                 if(plansza[i][j].czyo==0)
                 {
@@ -40,26 +46,62 @@ int main()
             printf("\n");
         }
         printf("\n");
-        if(pom==1)
+        //sprawdzanie czy nie roziwązalismy planszy
+        int koniec=18;
+        for(int i=0; i<18; i++)
+        {
+            if(tab[i]==0)
+            {
+                koniec--;
+            }
+        }// konczenie gry
+        if(koniec==18)
+        {
+            printf("Gratulacje wygrałeś w x ruchach");
+            return 0;
+            /*mieszanie();
+            for(int i=0; i<6; i++)
+            {
+                for(int j=0; j<6; j++)
+                {
+                    if(plansza[i][j].czyo==0)
+                    {
+                        printf("* ");
+                    }
+                    else
+                    {
+                        printf("%d ",plansza[i][j].val);
+                    }
+                }
+                printf("\n");
+            }
+            printf("\n");*/
+        }
+
+        if(pom==1)//jeśli to 2 z dwóch wybranych kafelków
         {
             x1=x;
             y1=y;
-            scanf("%d %d",&x,&y);
+            scanf("%d %d",&x,&y);//skanowanie 2 kafelka
             if(plansza[x][y].czyo==0)
             {
-               plansza[x][y].czyo=1;
+                plansza[x][y].czyo=1;
             }
-            else
+            else// skanowanie 2 kafelka dopóki będzie prawidłowy
             {
                 while(plansza[x][y].czyo==1)
-                {scanf("%d %d",&x,&y);}
+                {
+                    printf("wybierz zakryty kafelek\n");
+                    scanf("%d %d",&x,&y);
+                }
+                plansza[x][y].czyo=1;
             }
-            if(plansza[x][y].val!=plansza[x1][y1].val)
+            if(plansza[x][y].val!=plansza[x1][y1].val)//sprawdzanie czy są takie same
             {
                 plansza[x1][y1].czyo=1;
-                for(int i=0;i<6;i++)
+                for(int i=0; i<6; i++)
                 {
-                    for(int j=0;j<6;j++)
+                    for(int j=0; j<6; j++)
                     {
                         if(plansza[i][j].czyo==0)
                         {
@@ -76,29 +118,34 @@ int main()
                 plansza[x][y].czyo=0;
                 plansza[x1][y1].czyo=0;
             }
+            else
+            {
+                tab[plansza[x][y].val]=1;
+            }
             pom=0;
         }
-        else
+        else//jeśli to pierwszy z dwóch wybranych kafelków
         {
             scanf("%d %d",&x,&y);
             if(plansza[x][y].czyo==0)
             {
-               plansza[x][y].czyo=1;
+                plansza[x][y].czyo=1;
             }
             else
             {
                 while(plansza[x][y].czyo==1)
-                {scanf("%d %d",&x,&y);}
+                {
+                    printf("wybierz zakryty kafelek\n");
+                    scanf("%d %d",&x,&y);
+                }
+                plansza[x][y].czyo=1;
             }
             pom++;
         }
-
-
+        //zamknięcie gry
         if(x==-1 && y==-1)
         {
-           return 0;
+            return 0;
         }
-
     }
-
 }
